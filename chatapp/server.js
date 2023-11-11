33f30197f72228c6488fd265bbcbff13654ea661
swethasamy <75251257+swethasamy@users.noreply.github.com>
2023-11-11 19:31:54 +0530

    Add files via upload

---
+++ chatapp/server.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
